8cef2c971441d818dc7445d51e9f1d4dee2ec24a
Kyung-Tae <kim1@iastate.edu>
2020-01-30 15:56:21 -0600

    loadtest.cpp

--- 487/loadtest.cpp
+++ 487/loadtest.cpp
@@ -5,27 +5,30 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-   const int NUM_ELEMENTS = 5000000;
-   int clicks, lc, curr;
-   float secs;
-   vector<int> vec;
-   auto t1 = std::chrono::high_resolution_clock::now();
 
-   // Push values to vector
-   for(lc = 0; lc < NUM_ELEMENTS; lc++)
-   {
-      vec.push_back(lc);
-   } // for
+	auto t1 = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 100000; ++i) {
+		const int NUM_ELEMENTS = 5000000;
+		int clicks, lc, curr;
+		float secs;
+		vector<int> vec;
 
-   // Access and assignment (using [] operator)
-   for(lc = 0; lc < NUM_ELEMENTS; lc++)
-   {
-      curr = vec[lc];
-   } // for
+		// Push values to vector
+		for(lc = 0; lc < NUM_ELEMENTS; lc++)
+		{
+			vec.push_back(lc);
+		} // for
 
-   clicks = clock();
-   secs   = ((float)clicks)/CLOCKS_PER_SEC;
-   auto t2 = std::chrono::high_resolution_clock::now();
-   cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count() << endl;
-   return 0;
+		// Access and assignment (using [] operator)
+		for(lc = 0; lc < NUM_ELEMENTS; lc++)
+		{
+			curr = vec[lc];
+		} // for
+
+		clicks = clock();
+		secs   = ((float)clicks)/CLOCKS_PER_SEC;
+	}
+	auto t2 = std::chrono::high_resolution_clock::now();
+	cout << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count() << endl;
+	return 0;
 } // main 
