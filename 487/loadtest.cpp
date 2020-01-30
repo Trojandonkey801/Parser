0c49a39eef8bd51264b0f4af408a64bd3dbeb6b8
Kyung-Tae <kim1@iastate.edu>
2020-01-30 15:44:00 -0600

    loadtest.cpp

--- 487/loadtest.cpp
+++ 487/loadtest.cpp
@@ -1,31 +1,38 @@
-#include<pthread.h>
-#include<semaphore.h>
-#include<iostream>
+#include <iostream>
+#include <thread>
 using namespace std;
-sem_t a,b,c,d;
-void* load(void *input){
-	int array_to_add[20000];
-	for (int i = 0; i < 20000; ++i) {
-		array_to_add[i]+=2000;
-		while(array_to_add[i]>0){
-			array_to_add[i] /= 2;
-		}
-	std::cout << array_to_add[i] << std::endl;
-	}
-	return NULL;
-}
-pthread_t *e,*f,*g,*h;
-int main(int argc, char *argv[])
+
+void func_dummy(int N)
+ {
+   for (int i = 0; i < N; i++) {
+   cout << "Thread 1 :: callable => function pointer\n";
+   }
+ }
+ 
+class thread_obj {
+ public:
+   void operator()(int n) {
+       for (int i = 0; i < n; i++)
+           cout << "Thread 2 :: callable => function object\n";
+   }
+};
+ 
+int main()
 {
-	cout << "aasdfads" << endl;
-	pthread_create(e,NULL,load,NULL);
-	pthread_create(f,NULL,load,NULL);
-	pthread_create(g,NULL,load,NULL);
-	pthread_create(h,NULL,load,NULL);
-	pthread_join(*e,NULL);
-	pthread_join(*f,NULL);
-	pthread_join(*g,NULL);
-	pthread_join(*h,NULL);
-	return 0;
+auto f = [](int n) {
+   for (int i = 0; i < n; i++)
+   cout << "Thread 3 :: callable => lambda expression\n";
+   };
+thread th1(func_dummy, 2);
+ 
+thread th2(thread_obj(), 2);
+ 
+thread th3(f, 2);
+ 
+ th1.join();
+th2.join();
+ 
+th3.join();
+ 
+return 0;
 }
-	
